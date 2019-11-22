8a78e80c7be0cd741d986562dd4ae1fecfacbeed
YIueil <511210125@qq.com>
2019-11-22 15:13:17 +0800

    你好 github

---
+++ helloworld/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * message
+ * __   __ ____            _  __
+ * \ \ / /|_ _| __ __ ___ (_)| |
+ * \ V /  | | | || |/ -_)| || |
+ * |_|  |___| \_,_|\___||_||_|
+ * create by YIueil on time 2019/11/22
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("你好 世界");
+    }
+}
