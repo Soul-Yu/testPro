f9415b7cd91d92b6dbd59c79a64cdbd1ee9eb2f1
Soul-Yu <599792868@qq.com>
2019-11-22 13:20:27 +0800

    添加了几行代码

--- Believe/src/cn/edu/gues/BelieveYouself.java
+++ Believe/src/cn/edu/gues/BelieveYouself.java
@@ -3,6 +3,8 @@ package cn.edu.gues;
 public class BelieveYouself {
 	public static void main(String[] args) {
 		System.out.println("believe youself can do it ");
+		System.out.println("if you want to givre up ");
+		System.out.println("think everything fo you  you have nothing");
 	}
 
 }
